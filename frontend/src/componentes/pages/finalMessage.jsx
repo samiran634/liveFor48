e37547322109b8f64169e677d32eb75d43408579
Afham Shakeel <shakeelafham@gmail.com>
2025-10-19 18:43:04 +0530

    Small Changes to Landing and Final page

---
+++ frontend/src/componentes/pages/finalMessage.jsx
@@ -0,0 +1,14 @@
+export default function FinalMessage() {
+  return (
+    <>
+      <div
+        className="absolute inset-0 bg-cover bg-center animate-fadeInBackground"
+        style={{
+          backgroundImage:
+            'url("https://liftedicons.com/wp-content/uploads/2016/12/Dystopian-City-SNK-vs-Capcom-SVC-Chaos-.gif")',
+          backgroundAttachment: "fixed",
+        }}
+      />
+    </>
+  );
+}
